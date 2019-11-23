b5cdf465fb0c16588c00dbfe0ccd1e97fe02e167
unknown <muffakham.ali@gmail.com>
2019-11-22 21:14:53 -0700

    added the code for hostspot analysis.

--- Phase2/src/main/scala/cse512/HotcellUtils.scala
+++ Phase2/src/main/scala/cse512/HotcellUtils.scala
@@ -47,5 +47,65 @@ object HotcellUtils {
     return calendar.get(Calendar.DAY_OF_MONTH)
   }
 
+  def squaredValue (x: Int): Double = 
+  {
+    return (x*x).toDouble
+  } 
+
+   def gStatistic(x: Int, y: Int, z: Int, avg:Double, stdDev: Double, n: Int, sumN: Int, numOfCells: Int): Double =
+  {
+    val num = (sumN.toDouble - (avg*n.toDouble))
+    val value1 = ((numOfCells.toDouble*n.toDouble) -(n.toDouble*n.toDouble)).toDouble
+    val value2 = (numOfCells.toDouble-1.0).toDouble
+    val den = stdDev*math.sqrt((value1/value2)).toDouble
+    val result = (num/den).toDouble
+    return result
+  }
+
+  def Neighbours(minX: Int, minY: Int, minZ: Int, maxX: Int, maxY: Int, maxZ: Int, inputX: Int, inputY: Int, inputZ: Int): Int = 
+  {
+    var count = 0;
+    
+      
+    if (inputX == minX || inputX == maxX) {
+      count += 1;
+    }
+
+    if (inputY == minY || inputY == maxY) {
+      count += 1;
+    }
+
+    if (inputZ == minZ || inputZ == maxZ) {
+      count += 1;
+    }
+
+    if (count == 1) {
+      // Here only one of the cordinates is at its extremes, hence the point lies on the faces of the cube
+      val k = 17
+      return k;
+     
+    } 
+    else if (count == 2)
+    {
+      // Here two of the cordinates are at their extremes, hence the point lies on the edges of the cube
+      val k = 11
+      return k;
+      
+    }
+    else if (count == 3)
+    {
+      // the top right most corner of the lat,long,time CUBE
+      val k = 7
+      return k;
+    } 
+    else
+    {
+      //Here the point is inside the body of the cube
+       val k = 26
+      return k;
+      
+    }
+  }
+
   // YOU NEED TO CHANGE THIS PART
 }
